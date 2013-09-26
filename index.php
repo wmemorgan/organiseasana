2aeb35614f9271fb7b0c825946a1e80d6a7683ee
Michael Houston <mike@stashmetrics.com>
2013-09-26 23:28:02 +0100

    Added basic instructions and links

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include "asana.php";
 ?>
 <html>
 	<head>
-		<title>Organise Asana</title>
+		<title>Organise Asana Projects</title>
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 
@@ -34,7 +34,12 @@ include "asana.php";
 	</head>
 	<body>
 		<div class="container">
-			<h1>Organise Asana</h1>
+			<div class="page-header">
+				<h1>Organise Asana Projects <small><a href="http://kothar.net/projects/organise-asana.html">Help!</a></small></h1>
+			</div>
+			<p class="lead">
+				Copy <a href="https://asana.com">Asana</a> projects from one workspace to another.
+			</p>
 			<form method="POST">
 				<p>
 					<label>API Key: <input type="text" name="apiKey" value="<?php echo $apiKey ?>"></label>
@@ -110,6 +115,17 @@ include "asana.php";
 
 				} ?>
 			</form>
+
+			<div class="alert alert-info">
+				<h3>Source code</h3>
+				<p>Source code for this tool can be found at <a href="https://bitbucket.org/mikehouston/organiseasana">https://bitbucket.org/mikehouston/organiseasana</a></p>
+				<p>The implementation of the copy operation is based on <a href="https://gist.github.com/AWeg/5814427">https://gist.github.com/AWeg/5814427</a></p>
+				<h3>Privacy</h3>
+				<p>No data is stored on the server - the API key is not retained between calls. No cookies are stored.</p>
+				<h3>No Warranty</h3>
+				<p>This tool does not delete any data, and will not modifiy any existing projects (a new copy is made each time)</p>
+				<p>No warranty is made however - use at your own risk</p>
+			</div>
 		</div>
 	</body>
 </html>
