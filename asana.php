17042e88398193ded9ffda512422e6af3d9bc705
Mike Houston <mike@kothar.net>
2016-03-19 16:44:56 +0000

    Fix for large integers being formatted in scientific notation and losing precision.

--- asana.php
+++ asana.php
@@ -64,8 +64,25 @@ function asanaRequest($methodPath, $httpMethod = 'GET', $body = null, $cached = 
 
 		$data = curl_exec($ch);
 		$error = curl_error($ch);
+		$result = null;
 
-		$result = json_decode($data, true);
+		// See http://stackoverflow.com/a/27909889/37416
+		if (version_compare(PHP_VERSION, '5.4.0', '>=') && !(defined('JSON_C_VERSION') && PHP_INT_SIZE > 4)) {
+		    /** In PHP >=5.4.0, json_decode() accepts an options parameter, that allows you
+		     * to specify that large ints (like Steam Transaction IDs) should be treated as
+		     * strings, rather than the PHP default behaviour of converting them to floats.
+		     */
+		    $result = json_decode($data, true, 512, JSON_BIGINT_AS_STRING);
+		} else {
+		    /** Not all servers will support that, however, so for older versions we must
+		     * manually detect large ints in the JSON string and quote them (thus converting
+		     *them to strings) before decoding, hence the preg_replace() call.
+		     */
+		    $max_int_length = strlen((string) PHP_INT_MAX) - 1;
+		    $json_without_bigints = preg_replace('/:\s*(-?\d{'.$max_int_length.',})/', ': "$1"', $data);
+		    $result = json_decode($json_without_bigints, true);
+		}
+		// $result = json_decode($data, true);
 
 		if(isset($result['retry_after'])) {
 			$ratelimit = time() + $result['retry_after'];
